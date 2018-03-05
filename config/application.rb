ea7bbc9b1300bcf743da04f40ed637aedfebb2af
Gary Iams <ge.iams@gmail.com>
2018-03-05 16:49:11 -0500

    Instate a coding standard: Ruby Style Guide

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,14 @@
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
 # require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -16,11 +16,14 @@ require "action_cable/engine"
 Bundler.require(*Rails.groups)
 
 module Weather
+  # :nodoc:
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
+    # Settings in config/environments/* take precedence over those
+    # specified here.
+
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
